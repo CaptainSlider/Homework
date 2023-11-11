ec97249a1699abf855da793b8261cbf9b009cb67
CapitanSlider <d-a-n-max@yandex.ru>
2023-11-11 20:20:44 +0500

    (Chapter9) Apply First Task in Chapter 9

---
+++ Chapter9/Task in the end/Fisrt Task/BlockIntegration/BlockIntegration.cpp
@@ -0,0 +1,161 @@
+#include "pch.h"
+#include "BlockIntegration.h"
+#include <intrin.h>
+
+DRIVER_UNLOAD BlockIntegrationUnload;
+OB_PREOP_CALLBACK_STATUS OnPreOpenProcess(PVOID Context, POB_PRE_OPERATION_INFORMATION OperationInformation);
+extern "C"
+NTSTATUS ZwQueryInformationProcess(
+	_In_      HANDLE           ProcessHandle,
+	_In_      PROCESSINFOCLASS ProcessInformationClass,
+	_Out_     PVOID            ProcessInformation,
+	_In_      ULONG            ProcessInformationLength,
+	_Out_opt_ PULONG           ReturnLength
+);
+extern "C"
+char* PsGetProcessImageFileName(PEPROCESS eprocess);
+NTSTATUS IsDebug(PEPROCESS Process, bool* debug);
+PVOID RegistrationHandle;
+
+extern "C"
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING) {
+	auto status = STATUS_SUCCESS;
+
+	OB_OPERATION_REGISTRATION operation[] = {
+		PsProcessType,
+		OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE,
+		OnPreOpenProcess,
+		nullptr
+	};
+
+	OB_CALLBACK_REGISTRATION reg = {
+		OB_FLT_REGISTRATION_VERSION,
+		1,
+		RTL_CONSTANT_STRING(L"1212121.121212"),
+		nullptr,
+		operation
+	};
+
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\BlockIntegration");
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\BlockIntegration");
+	PDEVICE_OBJECT DeviceObject;
+	bool CreateSymlink = false;
+	do
+	{
+		status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, false, &DeviceObject);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("failed to create device (status = %08X)", status));
+			break;
+		}
+		status = IoCreateSymbolicLink(&symLink, &devName);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("failed to create symbol link (status = %08X)", status));
+			break;
+		}
+		CreateSymlink = true;
+		
+		status = ObRegisterCallbacks(&reg, &RegistrationHandle);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("failed to register callback (status = %08X)", status));
+			break;
+		}
+	} while (false);
+
+	if (!NT_SUCCESS(status)) {
+		if (DeviceObject) {
+			IoDeleteDevice(DeviceObject);
+		}
+		if (CreateSymlink) {
+			IoDeleteSymbolicLink(&symLink);
+		}
+	}
+	DriverObject->DriverUnload = BlockIntegrationUnload;
+	return status;
+}
+
+void BlockIntegrationUnload(PDRIVER_OBJECT DriverObject) {
+	ObUnRegisterCallbacks(RegistrationHandle);
+
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\BlockIntegration");
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
+}
+
+
+
+OB_PREOP_CALLBACK_STATUS OnPreOpenProcess(PVOID, POB_PRE_OPERATION_INFORMATION OperationInformation) {
+	if (OperationInformation->KernelHandle) {
+		return OB_PREOP_SUCCESS;
+	}
+
+	auto process = static_cast<PEPROCESS>(OperationInformation->Object);
+	auto currentProcess = PsGetCurrentProcess();
+	if (currentProcess == process) {
+		return OB_PREOP_SUCCESS;
+	}
+
+	//if (strcmp(PsGetProcessImageFileName(process), "msedge.exe") == 0 && strcmp(PsGetProcessImageFileName(currentProcess), "msedge.exe")) {
+	//	KdPrint(("Current: %s\n", PsGetProcessImageFileName(currentProcess)));
+	//	KdPrint(("PROCESS: %s\n", PsGetProcessImageFileName(process)));
+	//}
+	
+
+	if (OperationInformation->Operation == OB_OPERATION_HANDLE_CREATE && (OperationInformation->Parameters->CreateHandleInformation.DesiredAccess & RemoteMask)) {
+		bool debug = false;
+		auto status = IsDebug(process, &debug);
+		if (!NT_SUCCESS(status)) {
+			KdPrint(("Failed to IsDebug (status=%08X)", status));
+			return OB_PREOP_SUCCESS;
+		}
+
+		if (debug) {
+			return OB_PREOP_SUCCESS;
+		}
+
+		OperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~RemoteMask;
+	}
+	else {
+		if (OperationInformation->Parameters->DuplicateHandleInformation.DesiredAccess & RemoteMask) {
+			bool debug = false;
+			auto status = IsDebug(process, &debug);
+			if (!NT_SUCCESS(status)) {
+				KdPrint(("Failed to IsDebug (status=%08X)", status));
+				return OB_PREOP_SUCCESS;
+			}
+
+			if (debug) {
+				return OB_PREOP_SUCCESS;
+			}
+			OperationInformation->Parameters->DuplicateHandleInformation.DesiredAccess &= ~RemoteMask;
+		
+		}
+	}
+	
+
+	return OB_PREOP_SUCCESS;
+}
+
+NTSTATUS IsDebug(PEPROCESS Process,bool* debug) {
+	HANDLE hProcess;
+	*debug = false;
+	auto status = ObOpenObjectByPointer(Process, OBJ_KERNEL_HANDLE, nullptr, READ_CONTROL, nullptr, KernelMode, &hProcess);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to open object (status=%08X)", status));
+		return status;
+	}
+
+	PULONG debugPort = nullptr;
+	ULONG returnLength = 0;
+	status = ZwQueryInformationProcess(hProcess, ProcessDebugPort, &debugPort, sizeof(debugPort), &returnLength);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("Failed to check debug (status=%08X)", status));
+		return status;
+	}
+
+
+	if (debugPort) {
+		*debug = true;
+	}
+	return STATUS_SUCCESS;
+}
---
+++ Chapter9/Task in the end/Fisrt Task/Test/main.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+
+int Error(const char* msg) {
+    printf("%s Erorr= %d\n", msg, GetLastError());
+    return 1;
+}
+int wmain(int argc,wchar_t* argv[])
+{
+    if (argc < 1) {
+        return 1;
+    }
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, _wtoi(argv[1]));
+    if (hProcess == NULL)
+    {
+        return Error("Failed to open Process");
+    }
+    HMODULE Module = LoadLibraryA("user32.dll");
+    FARPROC loadLibraryAddress = GetProcAddress(Module, "MessageBoxW");
+    if (!loadLibraryAddress) {
+        return Error("Failed to ger proc adress");
+    }
+    LPVOID pRemoteParam = VirtualAllocEx(hProcess, NULL, 256, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (pRemoteParam == NULL)
+    {
+        CloseHandle(hProcess);
+        return Error("Failed to Allocate");
+    }
+
+    const wchar_t pMessage[] = L"Hello, world!";
+    bool statusWrite = WriteProcessMemory(hProcess, pRemoteParam, pMessage, wcslen(pMessage) * sizeof(wchar_t), NULL);
+    if (!statusWrite) {
+        return Error("Failed to write process memory");
+    }
+    
+    while (CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddress, pRemoteParam, 0, NULL) != NULL) {
+        printf("Driver not work");
+    }
+    printf("Driver Work");
+
+    VirtualFreeEx(hProcess, pRemoteParam, 0, MEM_RELEASE);
+    CloseHandle(hProcess);
+
+    return 0;
+}
